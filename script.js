ef344d0a46764075e9b4ac870bd0f41d973f3cb7
Udayraj <udayraj.m@bizamica.com>
2021-08-11 20:29:47 +0530

    Test assignment of string and integer

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 // start with strings, numbers and booleans
+let age = 100;
+let age2 = 100;
+console.log(age,age2);
+age = 200;
+console.log(age,age2);
 
+let name = 'Wes'
+let name2 =  name;
+console.log(name,name2);
+name = "Wes Lee"
+console.log(name,name2);
 // Let's say we have an array
 const players = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
 
