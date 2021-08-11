3ef93c85f5c0ce74a19398ef68a4e52e40645dd0
Udayraj <udayraj.m@bizamica.com>
2021-08-11 20:39:43 +0530

    Copy array using slice,concat,...spread and Array.from

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 // start with strings, numbers and booleans
-let age = 100;
-let age2 = 100;
-console.log(age,age2);
-age = 200;
-console.log(age,age2);
+// let age = 100;
+// let age2 = 100;
+// console.log(age,age2);
+// age = 200;
+// console.log(age,age2);
 
-let name = 'Wes'
-let name2 =  name;
-console.log(name,name2);
-name = "Wes Lee"
-console.log(name,name2);
+// let name = 'Wes'
+// let name2 =  name;
+// console.log(name,name2);
+// name = "Wes Lee"
+// console.log(name,name2);
+
 // Let's say we have an array
 const players = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
+const team = players;
+
+// console.log(players, team);
+// team[3] = 'Lux';
+console.log(players, team);
 
 // and we want to make a copy of it.
 
@@ -26,17 +32,21 @@ const players = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
 // Why? It's because that is an array reference, not an array copy. They both point to the same array!
 
 // So, how do we fix this? We take a copy instead!
-
+    
 // one way
+    const team2 = players.slice();
+    team[3] = 'Lux';
+    console.log(players, team2);
 
 // or create a new array and concat the old one in
-
+    const team3 = [].concat(players);
 // or use the new ES6 Spread
+    const team4 = [...players];
 
 // now when we update it, the original one isn't changed
-
+    team4[3] = "pikachu"
 // The same thing goes for objects, let's say we have a person object
-
+    const team5 = Array.from(players);
 // with Objects
 const person = {
     name: 'Wes Bos',
